e942240fb5cad0edf8f12a065a5b0dc35beec8ff
Autofilter004 <87107399+Autofilter004@users.noreply.github.com>
2021-08-07 21:44:41 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -125,9 +125,15 @@ async def auto_filter(bot, update):
                 file_link = f"https://t.me/{bot_.username}?start={unique_id}"
             
             results.append(
+
                 [
-                    InlineKeyboardButton(button_text, url=file_link)
+
+                    InlineKeyboardButton(button_text, url=file_link),
+
+                    InlineKeyboardButton(file_size, url=file_link),
+
                 ]
+
             )
         
     else:
