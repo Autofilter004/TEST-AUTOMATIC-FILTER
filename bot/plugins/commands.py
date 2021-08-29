ee663ebe626525605a32aa8312815026cba9aacf
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-08-29 12:47:16 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -125,37 +125,8 @@ Join on our channel to get movies ✅
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
+   
     
-    await bot.send_message(
-
-        chat_id=update.chat.id,
-
-        text=Translation.HELP_TEXT,
-
-        reply_markup=reply_markup,
-
-        parse_mode="html",
-
-        reply_to_message_id=update.message_id
-
-    )
-    
-    await bot.send_photo(
-
-        chat_id=update.chat.id,
-
-        photo = 'https://telegra.ph/file/f48f62bc9d3194c429a85.jpg',
-
-        caption=Translation.START_TEXT.format(
-
-                update.from_user.first_name),
-
-        parse_mode="html",
-
-        reply_to_message_id=update.message_id
-
-    )
-
 
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
@@ -175,6 +146,23 @@ async def help(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
+
+await bot.send_photo(
+
+        chat_id=update.chat.id,
+
+        photo = 'https://telegra.ph/file/f48f62bc9d3194c429a85.jpg',
+
+        caption=Translation.START_TEXT.format(
+
+                update.from_user.first_name),
+
+        parse_mode="html",
+
+        reply_to_message_id=update.message_id
+
+    )
+
 
 
 @Client.on_message(filters.command(["about"]) & filters.private, group=1)
